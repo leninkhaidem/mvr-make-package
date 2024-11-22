0d150bbe5e5ebb837b1bda3a50d737f0ae36ba2b
Lenin Khaidem <leninkhaidem@gmail.com>
2024-11-22 13:10:54 +0400

    fix: Added docstrings for the Template Engine, updated correlator return data type hint

--- mvr_make_package/mvr-template/{{cookiecutter.__package_name}}/python/{{cookiecutter.__module_name}}/correlators/c_1.py
+++ mvr_make_package/mvr-template/{{cookiecutter.__package_name}}/python/{{cookiecutter.__module_name}}/correlators/c_1.py
@@ -1,8 +1,11 @@
 
-from mvr_v2.common.utils import Correlator
+import typing
+
+from mvr_v2.common.utils import Correlator, PayloadInstance
 
 
 class MyCorrelator(Correlator):
-    def execute(self, data) -> list:
+    def execute(self, data: dict) -> typing.List[PayloadInstance]:
+        self.logger.info(data)  # remove this line
         # your correlation logic here
         return []
--- mvr_make_package/mvr-template/{{cookiecutter.__package_name}}/python/{{cookiecutter.__module_name}}/template_engines.py
+++ mvr_make_package/mvr-template/{{cookiecutter.__package_name}}/python/{{cookiecutter.__module_name}}/template_engines.py
@@ -9,28 +9,40 @@ A single template engine is equivalent to a single commit operation.
 
 
 class Te1(TemplateEngine):
+    """
+    This class holds NSO `xml` templates to be applied.
+    Multiple templates can be specified, with each instance corresponding to a single commit operation.
+    `mvr` will iteratively apply the specified template(s) for each normalized correlated payload.
+    """
+
     def settings(self):
+        """
+        This callback method is invoked by MVR to retrieve the names of the templates.
+        The template names should be assigned to the `self.templates` attribute as a list of strings.
+        """
         # you can use multiple templates
         self.templates = ["{{ cookiecutter.__package_name }}-template"]
 
     def format_single_commit_comment(self, _input) -> str:
         # return _input.device_name
         return 'single commit for {{ cookiecutter.__package_name }}'
-
-    def format_splitted_commit_comment(self, _single_payload_instance) -> str:
-        # return _single_payload_instance.interface_id
-        return 'splitted commit for {{ cookiecutter.__package_name }}'
 
 
 class Te2(TemplateEngine):
+    """
+    This class holds NSO `xml` templates to be applied.
+    Multiple templates can be specified, with each instance corresponding to a single commit operation.
+    `mvr` will iteratively apply the specified template(s) for each normalized correlated payload.
+    """
+
     def settings(self):
+        """
+        This callback method is invoked by MVR to retrieve the names of the templates.
+        The template names should be assigned to the `self.templates` attribute as a list of strings.
+        """
         # you can use multiple templates
         self.templates = ["{{ cookiecutter.__package_name }}-template"]
 
     def format_single_commit_comment(self, _input) -> str:
         # return _input.device_name
         return 'single commit for {{ cookiecutter.__package_name }}'
-
-    def format_splitted_commit_comment(self, _single_payload_instance) -> str:
-        # return _single_payload_instance.interface_id
-        return 'splitted commit for {{ cookiecutter.__package_name }}'
