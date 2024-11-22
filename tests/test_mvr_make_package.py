f5400f32cfa3775b912c9c361d3726babe9d878e
Lenin Khaidem <leninkhaidem@gmail.com>
2024-11-22 13:26:02 +0400

    test: updated test cases for v0.0.4

--- tests/test_mvr_make_package.py
+++ tests/test_mvr_make_package.py
@@ -1,6 +1,7 @@
 import hashlib
 import os
 import pathlib
+import re
 import shutil
 import subprocess
 
@@ -18,16 +19,17 @@ EXPECTED_FILES = {
     "package-meta-data.xml": "f208bdaeb5a8e500b057a3ae89513c503b2aba60a64332e251a21e91b5b8434c",
     "python/mvr_package_name/__init__.py": "6693de99707265d8fcafd204fe255f6a889698602853e8f04ee0d3d68739e7f0",
     "python/mvr_package_name/correlators/__init__.py": "5237e86ddee7798f0799c55f6d1076107195cb3485bb7101824a9a9cb4a3f4ad",
-    "python/mvr_package_name/correlators/c_1.py": "344c79f0a3533f65644df6a29d636019eeb4231a56647844edc17b3fb6877c09",
+    "python/mvr_package_name/correlators/c_1.py": "6be4b0e3d9fb3063d526adc450c40d09f7241d31ce1a8d9d58ad038aa486c300",
     "python/mvr_package_name/extractions/__init__.py": "ad68b66ca46abb5bb7a2b24991233040ae28d485c0f6804b51ae1265c6fd9940",
     "python/mvr_package_name/extractions/dx_1.py": "51b85647e1a0511ed5c97f71fb6f025ae92918624ce56390bb4e9d03200ff3be",
     "python/mvr_package_name/extractions/dx_2.py": "03ee4f0bce66cc6d1950901e05b63c399cb3ffbfbf56764bb53b2627b5d5918f",
     "python/mvr_package_name/mvr_package_name.py": "9459613c8bc6f47c6aee88e2424ad9dc0ff8b53eba43665306482cce467b8d5c",
-    "python/mvr_package_name/template_engines.py": "ee04ca9123e8e1e2c2d40b5dfaae0b864e1862249e778e14cbcf3d65ae25fc5f",
+    "python/mvr_package_name/template_engines.py": "67fadb9cb20127cce3cbe8b7d379bb0fe021eb5d87fdfdb3fd6795f259058a99",
     "python/mvr_package_name/yang_inputs.py": "889acc11e52b654c77483976b8d96ee84f4c604b236ea42971847ad65fb04be0",
     "src/Makefile": "20d6355a62973fdca0e6fcc61e16e60a77468def252e7d993c6eecda0a76e943",
-    "src/yang/mvr-package-name.yang": "63a4da84409b628211a0f8a0daf8fad63b77e899f3c83d6bead45f6e05ac4fa8",
+    "src/yang/mvr-package-name.yang": "dd29eda7b43c05ec9a6addddb4228a4f9185b92be6470c25b1a65824773e8060",
     "templates/mvr-package-name-template.xml": "4461c12fe9b8f71d57ee939820b87dda9452605848212330bf122c4270fab723",
+
 }
 
 
@@ -56,9 +58,22 @@ def generate_package():
     # Verify the package directory was created
     assert os.path.isdir(
         BASE_PATH), f"Failed to generate package at {BASE_PATH}"
+    # Replace date string in yang file
+    yang_file_path = os.path.join(BASE_PATH, "src/yang/mvr-package-name.yang")
+    if os.path.isfile(yang_file_path):
+        replace_date_string_to_xxx(yang_file_path)
     yield
     if os.path.exists(BASE_PATH):
         shutil.rmtree(BASE_PATH)
+
+
+def replace_date_string_to_xxx(file_path):
+    with open(file_path, "r") as f:
+        content = f.read()
+    # replace with regex
+    content = re.sub(r"revision \d{4}-\d{2}-\d{2}", "revision xxxx", content)
+    with open(file_path, "w") as f:
+        f.write(content)
 
 
 @pytest.mark.parametrize("relative_path, expected_hash", EXPECTED_FILES.items())
